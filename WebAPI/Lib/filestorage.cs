122a61667e22480cc2043cc08265909591ea6e68
彥伯 李 <iainbo.lee@gmail.com>
2022-03-29 16:04:48 +0800

    新增total

---
+++ WebAPI/Lib/filestorage.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebAPI.Lib
+{
+    public class FileManager
+    {
+        public class FileMetadata
+        {
+            public string FullFileName { get; set; }
+            public string FileName { get; set; }
+            public string FileExtension { get; set; }
+            public string ContentType { get; set; }
+            public long ContentLen { get; set; }
+
+            public FileMetadata(IFormFile file)
+            {
+                FullFileName = file.FileName;
+                FileName = Path.GetFileNameWithoutExtension(file.FileName);
+                FileExtension = Path.GetExtension(file.FileName).Replace(".", "");
+                ContentType = file.ContentType;
+                ContentLen = file.Length;
+            }
+        }
+        public bool UploadFile(IFormFile file, int AID)
+        {
+            string FileStorage_Root = AppConfig.Config["Filestorage:Default"];
+
+            // 取App config
+            
+            // 取檔案的extension
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            if (string.IsNullOrEmpty(ext))
+            {
+                return false;
+            }
+
+            string HexStr = Convert.ToString(Convert.ToInt32(AID), 16).PadLeft(8, '0');
+            string SubFilePath = string.Join("\\", System.Text.RegularExpressions.Regex.Split(HexStr, "(?<=\\G.{2})(?!$)"));
+            string FilePath = Path.Combine(FileStorage_Root, SubFilePath);
+
+            FileInfo fi = new FileInfo(FilePath);
+            fi.Directory.Create();
+            using (FileStream fs = fi.Create())
+            {
+                file.CopyTo(fs);
+            }
+
+            return true;
+        }
+
+        public static async Task<MemoryStream> DownloadFile(int? AID)
+        {
+            string FileStorage_Root = AppConfig.Config["Filestorage:Default"];
+
+            
+
+            string HexStr = Convert.ToString(Convert.ToInt32(AID), 16).PadLeft(8, '0');
+            string SubFilePath = string.Join("\\", System.Text.RegularExpressions.Regex.Split(HexStr, "(?<=\\G.{2})(?!$)"));
+            string FilePath = Path.Combine(FileStorage_Root, SubFilePath);
+
+            if (!System.IO.File.Exists(FilePath))
+            {
+                throw new Exception("找不到該檔案！");
+            }
+
+            MemoryStream Result = new MemoryStream();
+
+            using (FileStream fs = new FileStream(FilePath, FileMode.Open))
+            {
+                await fs.CopyToAsync(Result);
+            }
+
+            Result.Seek(0, SeekOrigin.Begin);
+            return Result;
+        }
+    }
+    
+}
---
+++ WebAPI/Repository/NoteRepository.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Lib;
+
+namespace WebAPI.model
+{
+    public class NotrRepository
+    {
+        //public IEnumerable<Note>GetList(int id)
+        //{
+        //    string sqlstr = $@"SELECT * FROM V_Spot
+        //                    where CName like @City
+        //                    and(@Type is null or Type = @Type)
+        //                    order by OId
+        //                    offset (@page - 1) * @fetch rows
+        //                    fetch next @fetch rows only";
+            
+        //    var p = new DynamicParameters();
+
+        //    p.Add("id", id);
+
+        //    using (var db = new AppDb())
+        //    {
+        //        var result = db.Connection.Query<Spot>(sqlstr, p);
+        //        return result;
+        //    }
+        //}
+
+        
+
+
+
+        public Spot_detail Get(int id)
+        {
+            var sqlstr =
+                @"SELECT * FROM V_Spot_Detail where OID = @id";
+
+
+
+            using (var db = new AppDb())
+            {
+                var result = db.Connection.QueryFirstOrDefault<Spot_detail>(sqlstr, new { id });
+                return result;
+            }
+        }
+    }
+}
